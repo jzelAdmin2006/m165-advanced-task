303442c3919a40bc1e3cbf461cd344e7a4f99bb7
Johannes Zeller <johannes_zeller@sluz.ch>
2023-06-07 16:10:09 +0200

    solve task 1

--- db.py
+++ db.py
@@ -3,9 +3,19 @@ import os
 from pymongo import MongoClient
 
 
+CS_ENV = 'CS_MONGODB'
+DB_NAME = 'Softwares'
+COL_NAME = 'Keys'
+
 class Db:
     def __init__(self):
-        self.client = MongoClient(os.environ['CS_MONGODB'])
+        self.client = MongoClient(os.environ[CS_ENV])
+        self.db = self.client[DB_NAME]
+        self.col = self.db[COL_NAME]
 
-    def check_availability(self):
-        return 'Softwares' in self.client.list_database_names()
+    def initialize(self):
+        self.col.insert_one({})
+
+    def db_col_is_available(self):
+        return DB_NAME in self.client.list_database_names() and COL_NAME in self.db.list_collection_names()
+    
--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 from db import Db
 
-print(Db().check_availability())
+db = Db()
+
+def proceed():
+    print('Hello, World!')
+
+
+if db.db_col_is_available():
+    proceed()
+else:
+    while True:
+        createDbColInput = input('The database "Softwares" with the collection "Keys" does not exist. Do you wish to create them? [y] / [n]')
+        if createDbColInput == 'y':
+            db.initialize()
+            proceed()
+            break
+        elif createDbColInput == 'n':
+            break
+
+print('Terminating...')
